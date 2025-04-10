d71ce1b6f7624dd084e1e71e633732bfdd1f99d7
タイン・ヴィン <117455879+vinh12112004@users.noreply.github.com>
2025-04-10 17:26:52 +0700

    cau hinh env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000 
+
+require("dotenv").config()
+const port = process.env.PORT 
 const route = require("./routes/client/index.route")
 
 app.set("views","./views")
