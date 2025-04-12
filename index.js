54a4c0e40cc91a6d0bad9317aa9258c4d8022760
タイン・ヴィン <117455879+vinh12112004@users.noreply.github.com>
2025-04-12 16:01:24 +0700

    tao trang tong quan

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const database = require("./config/database")
 const route = require("./routes/client/index.route")
+const adminRoute = require("./routes/admin/index.route");
 require("dotenv").config();
 const app = express()
 
@@ -15,7 +16,9 @@ app.set("views","./views")
 app.set("view engine","pug")
 app.use(express.static("public"))
 
-route(app)
+adminRoute(app);
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
