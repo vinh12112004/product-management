bbac88ebb7351d37e3e6da1433113b35452865fc
タイン・ヴィン <117455879+vinh12112004@users.noreply.github.com>
2025-04-13 16:56:57 +0700

    lay sp vao trang san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,13 @@
 // [GET] /admin/products
 
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted : false
+    }).lean();
     res.render("admin/pages/products/index",{
-        pageTitle : "trang tong quan"
+        pageTitle : "trang tong quan",
+        products : products
     });
   }
