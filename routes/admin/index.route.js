5785ffdb1dfa82b9bc3d1dcc4a51291c234e8037
タイン・ヴィン <117455879+vinh12112004@users.noreply.github.com>
2025-04-13 14:01:27 +0700

    lam giao dien trang danh sach san pham

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,8 @@
 const dashboardRoutes = require("./dashboard.route.js");
 const systemConfig = require("../../config/system.js");
+const productsRoutes = require("./product.route.js")
 module.exports = (app) =>{
     const PATH_ADMIN = systemConfig.prefixAdmin;
     app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
+    app.use(PATH_ADMIN + "/products", productsRoutes);
 }
